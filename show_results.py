437afe4c178de78baf71a09ee8df2c8048407191
sdcotton <icottonsd@gmail.com>
2019-05-27 14:19:32 -0700

    kept function the same and reused for 2nd dataset

--- show_results.py
+++ show_results.py
@@ -55,34 +55,21 @@ def show_histogram(words_dict):
     plt.title('Russian Twitter Bot Word Frequency')
     plt.ylabel('Frequency (%)')
     plt.show()
-    
-def show_histograms(words1, words2):
-    assert isinstance(words1,dict)
-    assert isinstance(words2,dict)
+
+def show_histograms(words, title):
+    assert isinstance(words,dict)
+    assert isinstance(title,str)
     import matplotlib.pyplot as plt
 
     # Create list and sort in descending order
-    tups=list(words1.items())
+    tups=list(words.items())
     tups.sort(key=lambda tup: tup[1],reverse=True)  # sort by values
     m=list(map(list,zip(*tups)))  # Unzip tuples into 2 lists
     x=m[0]
     y=m[1]
-    plt.subplot(121)
     plt.bar(x, y, color='#ab4435')
     plt.xticks(rotation=90)
-    plt.title('Russian Twitter Bot Word Frequency')
-    plt.ylabel('Frequency (%)')
-    
-    # Do the same again for the second dict
-    tups = list(words2.items())
-    tups.sort(key=lambda tup: tup[1], reverse=True)
-    m = list(map(list, zip(*tups)))
-    x = m[0]
-    y = m[1]
-    plt.subplot(122)
-    plt.bar(x, y, color='#ab4435')
-    plt.xticks(rotation=90)
-    plt.title('Normal Tweet Word Frequency')
+    plt.title(title)
     plt.ylabel('Frequency (%)')
     plt.show()
 
@@ -94,6 +81,7 @@ def show_results():
     matched = match_error(normalWords, trollWords, ['vote', 'trump', 'hillari', 'hillary', 'clinton', 'amp'])
     print(matched)
     #show_wordcloud(words = words)
-    show_histograms(trollWords, normalWords)
+    show_histograms(trollWords,title='Russian Twitter Bot Word Frequency')
+    show_histograms(normalWords,title='User Political Tweet Word Frequency')
 if __name__=='__main__':
     show_results()
