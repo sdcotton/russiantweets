ccf45e43f3b8482370145ea4643d5f828f1fc317
sdcotton <icottonsd@gmail.com>
2019-05-27 14:21:27 -0700

    removed duplicate function;

--- show_results.py
+++ show_results.py
@@ -30,7 +30,7 @@ def show_wordcloud(words):
     plt.tight_layout(pad=0)
     plt.show()
 
-def show_histogram(words_dict):
+def show_histograms(words, title):
     '''
     :param words: Counter dictionary of words and their frequencies
     :type: dict
@@ -40,23 +40,6 @@ def show_histogram(words_dict):
     :x: words
     :y: number of occurances
     '''
-    # SETUP #
-    assert isinstance(words_dict,dict)
-    import matplotlib.pyplot as plt
-
-    # Create list and sort in descending order
-    tups=list(words_dict.items())
-    tups.sort(key=lambda tup: tup[1],reverse=True)  # sort by values
-    m=list(map(list,zip(*tups)))  # Unzip tuples into 2 lists
-    x=m[0]
-    y=m[1]
-    plt.bar(x, y, color='#ab4435')
-    plt.xticks(rotation=90)
-    plt.title('Russian Twitter Bot Word Frequency')
-    plt.ylabel('Frequency (%)')
-    plt.show()
-
-def show_histograms(words, title):
     assert isinstance(words,dict)
     assert isinstance(title,str)
     import matplotlib.pyplot as plt
