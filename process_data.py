2030daa0fc74161713dcab66b9ca11f7fb9cf870
sdcotton <icottonsd@gmail.com>
2019-05-31 17:02:56 -0700

    added comments

--- process_data.py
+++ process_data.py
@@ -1,5 +1,14 @@
 def match_samples(dict_one, dict_two, sample_list):
-    '''Generates a pairing between the two given dicts for each value in the given list'''
+    '''Generates a pairing between the two given dicts for each
+    value in the given list
+    :param dict_one: 1st dictionary to find words in
+    :type: dict
+    :param dict_two: 2nd dictionary to find words in
+    :type: dict
+    :sample_list: words to compare
+    :type: dict
+    :returns: dict
+    '''
     assert isinstance(dict_one, dict) and dict_one
     assert isinstance(dict_two, dict) and dict_two
     assert isinstance(sample_list, list) and sample_list
@@ -9,7 +18,17 @@ def match_samples(dict_one, dict_two, sample_list):
     return d
 
 def match_error(source_dict, other_dict, sample_list):
-    ''''''
+    '''
+    Creates dictionary with match_samples and takes the relative frequency
+    (v2-v1)/v1 instead of just comparing the two directly
+    :param source_dict: 1st dictionary to find words in
+    :type: dict
+    :param other_dict: 2nd dictionary to find words in
+    :type: dict
+    :param sample_list: words to compare
+    :type: list
+    :returns: dict
+    '''
     matched = match_samples(source_dict, other_dict, sample_list)
     d = {k:((v[1]-v[0])/v[0]) for k, v in matched.items()}
     return d
